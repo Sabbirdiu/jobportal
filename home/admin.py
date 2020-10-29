332738db7c077d7be8533e0467ae3125a4eed495
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-10-29 13:26:41 +0600

    Added admin in home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import JobListing,Category
+class CatAdmin(admin.ModelAdmin):
+    
+    prepopulated_fields = {'slug': ('title',)} 
+
+admin.site.register(Category,CatAdmin)
+admin.site.register(JobListing)
