ad28f11ae19d06c164e2c16046ce1df85db1e25d
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-10-29 14:06:04 +0600

    rendering data to template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Category,JobListing
+
 
 def index(request):
-    return render(request,"index.html")
+    qs = JobListing.objects.all()
+
+    context = {
+        'joblist': qs,
+    }
+
+    return render(request,"index.html",context)
