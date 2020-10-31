fcd4c1dcf114affe554305e564dbe0498e4446d6
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-10-31 22:42:59 +0600

    radded account app and added the signup view

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'home',
+    'account',
 ]
 
 MIDDLEWARE = [
--- jobportal/urls.py
+++ jobportal/urls.py
@@ -6,6 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('account/',include('account.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
