d1ce06d26b2454727f6c207360d3d7310d187cb9
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-11-06 12:13:14 +0600

    update account app

--- account/views.py
+++ account/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def signup(request):
     return render(request,'signup.html')
+
+def signin(request):
+    return render(request,'signin.html')
