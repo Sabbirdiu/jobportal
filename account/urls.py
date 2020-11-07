934c9a256503a2052fc188e06104485d888f0e51
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-11-07 22:04:45 +0600

    Added signin function inurl

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,8 @@ from .views import *
 urlpatterns = [
     # path('signup/',signup,name='signup'),
     path('employee/register', RegisterEmployeeView.as_view(), name='signup'),
-    path('signin/',signin,name='signin'),
+    # path('signin/',signin,name='signin'),
+    path('signin', signin.as_view(), name='signin'),
     path('employersignup/',employersignup,name='employersignup'),
     
 ]
