1c26483acf1b4286257ac274456ecb2dd53a0d65
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-11-09 00:06:45 +0600

    Added employer url

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,10 @@ from .views import *
 urlpatterns = [
     # path('signup/',signup,name='signup'),
     path('employee/register/', RegisterEmployeeView.as_view(), name='signup'),
+    path('employer/register', RegisterEmployerView.as_view(), name='employersignup'),
     # path('signin/',signin,name='signin'),
     path('signin', signin.as_view(), name='signin'),
-    path('employersignup/',employersignup,name='employersignup'),
+    # path('employersignup/',employersignup,name='employersignup'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
     
