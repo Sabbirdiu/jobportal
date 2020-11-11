caeb3ba994a41fcc9d096d546ddf231550a4e6d5
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-11-11 23:33:45 +0600

    job apply is now ready for work

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import JobListing,Category
+from .models import JobListing,Category,ApplyJob
 class CatAdmin(admin.ModelAdmin):
     
     prepopulated_fields = {'slug': ('title',)} 
@@ -9,6 +9,11 @@ class JobAdmin(admin.ModelAdmin):
     
     prepopulated_fields = {'slug': ('title',)}     
 
-
+class ApplyAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'email', 'file') 
+    list_filter = ('name', 'email') 
+    search_fields = ('name', 'email') 
 admin.site.register(Category,CatAdmin)
 admin.site.register(JobListing,JobAdmin)
+admin.site.register(ApplyJob,ApplyAdmin)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404,HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404,HttpResponseRedirect,redirect
 from django.http import HttpResponse
 from home.models import Category,JobListing,ApplyJob
 from django.contrib.auth.decorators import login_required
@@ -36,13 +36,14 @@ def jobdetails(request,slug):
        
     }
     return render(request,"jobdetails.html",context)
-@login_required
+@login_required(login_url='/signin')
 def apply_job(request):
     form = JobApplyForm(request.POST or None, request.FILES)
     if form.is_valid():
         instance = form.save()
         instance.save() 
         return redirect('/')
+        
     context = {
         'form': form,
 
