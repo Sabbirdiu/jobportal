ebf1f1c3a9881b1c2ef2617596c2e7c4a0509891
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-11-11 22:31:12 +0600

    Added apply job views

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404,HttpResponseRedirect
 from django.http import HttpResponse
-from home.models import Category,JobListing
+from home.models import Category,JobListing,ApplyJob
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
 
 def index(request):
@@ -34,5 +36,16 @@ def jobdetails(request,slug):
        
     }
     return render(request,"jobdetails.html",context)
+@login_required
+def apply_job(request):
+    form = JobApplyForm(request.POST or None, request.FILES)
+    if form.is_valid():
+        instance = form.save()
+        instance.save() 
+        return redirect('/')
+    context = {
+        'form': form,
 
+    } 
+    return render(request, "jobs/job_apply.html", context)
 
